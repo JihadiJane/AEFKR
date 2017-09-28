2d4ee8c4154eff1c93ee59ed3f982ba232c76376
Ben Cone <bencone@barium.themolecule.com>
2017-09-28 18:11:03 -0400

    psuedo code for timeFKR

--- AEFKR.jsx
+++ AEFKR.jsx
@@ -1,1 +1,23 @@
-alert("yo");
+var mainComp = app.project.activeItem;
+var selectedLayers = mainComp.selectedLayers;
+var numLayers = selectedLayers.length;
+
+var glitchFreq = 0; 
+var glitchAmp = 0;
+var frameBias = 2;
+var tempTime;
+
+
+// time fucker
+for (var i = 0; i < numLayers; i++)
+{
+	// get time length of each layer
+	// while tempTime < time length do this, else: tempTime = 0, 
+	// // tempTime += rand(frameBias, frameBias + rand(0, frameBias * (1 + amp)) % 2
+	// // place keyframe at tempTime
+
+	// repeat while loop for moving keyframes and converting some to holds
+	// convert to hold, then probability choose if moved (low probability for moving keys)
+
+}
+
