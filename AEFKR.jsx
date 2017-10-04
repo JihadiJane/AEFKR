2853e33d6fcceb04783802f496b61753666cf73e
JihadiJane <benjaminc.cone@gmail.com>
2017-10-04 00:38:55 -0400

    adds mask to shape layer, addValueAtTime() function to be tested later

--- AEFKR.jsx
+++ AEFKR.jsx
@@ -11,6 +11,24 @@ var tempTime = 0;
 // time fucker
 for (var i = 0; i < numLayers; i++)
 {
+
+	var currentLayer, verts, tempMask, newMask;
+
+	verts = [[0,0],[0,100],[100,100],[100,0]];
+
+	currentLayer = selectedLayers[i];
+
+	newShape = new Shape();
+	newShape.vertices = verts;
+
+	newMask = currentLayer.property("ADBE Mask Parade").addProperty("Mask");
+	newMask.property("ADBE Mask Shape").setValue(newShape);
+	newMask.name = "newMask_001";
+
+	prop1.setValueAtTime(seconds, value);
+	// tempMask = currentLayer.property("ADBE Mask Parade").property(1);
+	// tempMask.property("ADBE Mask Shape").setValue(verts);
+
 	// get time length of each layer
 	// while tempTime < time length do this, else: tempTime = 0, 
 	// // tempTime += rand(frameBias, frameBias + rand(0, frameBias * (1 + amp)) % 2
@@ -22,5 +40,5 @@ for (var i = 0; i < numLayers; i++)
 	glitchAmp = i;
 }
 
-alert(glitchAmp);
+alert(selectedLayers);
 
